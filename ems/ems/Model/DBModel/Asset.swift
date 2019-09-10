726a86cfe54627d148d33a3374b134de15535161
吉野瑠 <s15549@salesio-sp.ac.jp>
2019-09-10 20:30:11 +0900

    asset set func

--- ems/ems/Model/DBModel/Asset.swift
+++ ems/ems/Model/DBModel/Asset.swift
@@ -17,8 +17,8 @@ struct Asset {
     let discard: Bool?
     let location: String?
     let quantity: Int?
-    let createdDate: String
-    let updateDate: String
+    let createdDate: String?
+    let updateDate: String?
 
     enum AssetID: String {
         case code
@@ -35,6 +35,49 @@ struct Asset {
         var key: String {
             return self.rawValue
         }
+    }
+
+    var data: [AnyHashable: Any] {
+        get {
+            var asset: [AnyHashable: Any] = [:]
+            asset[AssetID.code.key] = code
+
+            if let name = name {
+                asset[AssetID.name.key] = name
+            }
+            if let admin = admin {
+                asset[AssetID.admin.key] = admin
+            }
+            if let user = user {
+                asset[AssetID.user.key] = user
+            }
+            if let loss = loss {
+                asset[AssetID.loss.key] = loss
+            }
+            if let discard = discard {
+                asset[AssetID.discard.key] = discard
+            }
+            if let location = location {
+                asset[AssetID.location.key] = location
+            }
+            if let quantity = quantity {
+                asset[AssetID.quantity.key] = quantity
+            }
+            return asset
+        }
+    }
+
+    init(code: String, name: String? = nil, admin: String? = nil, user: String? = nil, loss: Bool? = nil, discard: Bool? = nil, location: String? = nil, quantity: Int? = nil) {
+        self.code = code
+        self.name = name
+        self.admin = admin
+        self.user = user
+        self.loss = loss
+        self.discard = discard
+        self.location = location
+        self.quantity = quantity
+        self.createdDate = nil
+        self.updateDate = nil
     }
 
     init?(data: [String: Any]) {
