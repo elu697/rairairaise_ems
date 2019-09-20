30ed215b863408ab4b1682a659dce84cc0aa4ae2
hiza <hiza0506@gmail.com>
2019-09-20 13:53:12 +0900

    Fix Lint error.

--- ems/ems/Utility/UDManager.swift
+++ ems/ems/Utility/UDManager.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-class UDManager {
-    enum UDkey: String {
-        case sound = "sound"
+internal class UDManager {
+    internal enum UDkey: String {
+        case sound = "ud_sound"
     }
     
-    static func setUD(key: UDkey, value: Any) {
+    internal static func setUD(key: UDkey, value: Any) {
         let share = UserDefaults.standard
         share.set(value, forKey: key.rawValue)
         share.synchronize()
     }
     
-    static func getUD(key: UDkey) -> Any {
+    internal static func getUD(key: UDkey) -> Any {
         let share = UserDefaults.standard
         return share.object(forKey: key.rawValue) as Any
     }
