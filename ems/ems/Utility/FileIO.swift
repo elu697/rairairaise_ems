568d1bc0e01555294d10a24243b4f39c127a9b2c
吉野瑠 <s15549@salesio-sp.ac.jp>
2019-12-11 21:09:11 +0900

    csv load

---
+++ ems/ems/Utility/FileIO.swift
@@ -0,0 +1,38 @@
+//
+//  FileIO.swift
+//  ems
+//
+//  Created by 吉野瑠 on 2019/12/11.
+//  Copyright © 2019 RaiRaiRaise. All rights reserved.
+//
+
+import Foundation
+
+class FileIO {
+    static internal func load(fileName: String) -> [String]? {
+        var csvData: [String]? = nil
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let pathFileName = dir.appendingPathComponent(fileName)
+            do {
+                let csv = try String(contentsOf: pathFileName, encoding: .utf8)
+                csvData = csv.lines
+            } catch {
+                print("読み込み失敗")
+            }
+        }
+        return csvData
+    }
+    
+    static internal func save(data: Data, fileName: String) -> Bool {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let pathFileName = dir.appendingPathComponent(fileName)
+            do {
+                try data.write(to: pathFileName)
+            } catch {
+                print("書き込み失敗")
+                return false
+            }
+        }
+        return true
+    }
+}
